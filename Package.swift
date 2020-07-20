623533568223f5a9570390d3145ec914996a7454
Roc Zhang <roczhang9673@gmail.com>
2020-07-20 16:46:43 +0800

    feat: Adjust file structure and try to add package.swift for it

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.2
+import PackageDescription
+
+let package = Package(
+  name: "CollectionKit",
+  platforms: [.iOS(.v8)],
+  products: [
+    .library(name: "CollectionKit", targets: ["CollectionKit"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/lkzhao/YetAnotherAnimationLibrary.git", from: "1.4.0"),
+  ],
+  targets: [
+    .target(name: "CollectionKit", dependencies: ["YetAnotherAnimationLibrary"])
+  ]
+)
+
